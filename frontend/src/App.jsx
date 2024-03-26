c89fa7109744b562eaa8d661af5f99ecfeb4c6c4
sam <sfriedman6711@gmail.com>
2024-03-26 13:36:25 -0600

    front-end starter finished and working

---
+++ frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Navigation from "./components/Navigation/";
+import * as sessionActions from "./store/session";
+import { Modal } from "./context/Modal";
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true);
+    });
+  }, [dispatch]);
+
+  return (
+    <>
+      <Modal />
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && <Outlet />}
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <h1>Welcome!</h1>,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
