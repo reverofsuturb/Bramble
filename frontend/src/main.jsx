df29bd8195e389d1f7e8d2b485f5a348bc6c021e
sam <sfriedman6711@gmail.com>
2024-03-26 14:49:24 -0600

    render fixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
-import { ModalProvider, Modal } from './context/Modal';
+import { ModalProvider } from './context/Modal';
 
 const store = configureStore();
 
@@ -18,22 +18,12 @@ if (import.meta.env.MODE !== "production") {
   window.sessionActions = sessionActions;
 }
 
-// const Carrot = () => (
-//   <div style={{ color: "orange", fontSize: "100px" }}>
-//     <i className="fas fa-carrot"></i>
-//   </div>
-// );
 
-// Wrap the application with the Modal provider and render the Modal component
-// after the App component so that all the Modal content will be layered as
-// HTML elements on top of the all the other HTML elements:
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ModalProvider>
       <Provider store={store}>
         <App />
-        {/* <Carrot /> */}
-        {/* <Modal /> */}
       </Provider>
     </ModalProvider>
   </React.StrictMode>
